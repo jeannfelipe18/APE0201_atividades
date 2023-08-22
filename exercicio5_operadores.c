456098900a90e3514e8b498387543597fbd59780
jeannfelipe18 <135741976+jeannfelipe18@users.noreply.github.com>
2023-08-21 23:15:23 -0300

    Update exercicio5_operadores.c

--- exercicio5_operadores.c
+++ exercicio5_operadores.c
@@ -1,25 +1,42 @@
 #include<stdio.h>
 
+
 main(){
-    //Faça um programa em que o usuário digite o custo de uma determinada mercadoria, em seguida, adiciona-se ao custo o valor do frete e despesas eventuais (também solicitadas pelo teclado). Para concluir, o programa pergunta qual o valor de venda e indica a percentagem de lucro da mercadoria.
 
-    float custo, valorFrete, despesas, valorVenda, valorBruto, lucro, porcentagemLucro;
 
-    printf("\nDigite o valor de custo da mercadoria");
+    float custo, valorFrete, despesas, valorVenda, lucro, soma;
+   
+    printf("Digite o valor de custo da mercadoria:");
     scanf("%f", &custo);
 
-    printf("\nDigite o valor do frete da mercadoria");
+
+    printf("Digite o valor de frete da mercadoria:");
     scanf("%f", &valorFrete);
 
-    printf("\nDigite o valor do frete das despesas");
+
+    printf("Digite o valor do frete das despesas:");
     scanf("%f", &despesas);
 
-    printf("Digite o valor de venda");
+
+    printf("Digite o valor da venda:");
     scanf("%f", &valorVenda);
 
-    valorBruto = custo + valorFrete + despesas;
 
-    porcentagemLucro = (lucro/valorBruto)*100;
+    soma = (custo + valorFrete+ despesas);
+
+
+    lucro = (soma/ valorVenda)*100;
+
 
     printf("A porcentagem de lucro é: %.2f%%",lucro);
+
+
+
+
+
+
+
+
+
+
 }
